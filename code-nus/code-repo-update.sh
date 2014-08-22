0c0c2611783004a7773fa3b4703b7113c4b81d43
Yujian <yyjhao@gmail.com>
2014-08-22 16:37:13 +0800

    much edit

--- code-nus/code-repo-update.sh
+++ code-nus/code-repo-update.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 cd /home/git/code-nus-repos
 git pull
 npm install
 node update.js
-cp /home/git/code-nus-repos/data.json /var/www/code.nushackers.org/app/scripts/data.json
+cp /home/git/code-nus-repos/data.json /var/www/code.nushackers.org/scripts/data.json
--- code-nus/post-receive.sh
+++ code-nus/post-receive.sh
@@ -6,6 +6,7 @@ git --work-tree=/home/git/code-nus --git-dir=/home/git/code-nus.git checkout -f
 
 cd /home/git/code-nus
 npm install
+bower install
 gulp build
-cp -r dest/* /var/www/code.nushackers.org/
+cp -r dist/* /var/www/code.nushackers.org/
 cp /home/git/code-nus-repos/data.json /var/www/code.nushackers.org/app/scripts/data.json
